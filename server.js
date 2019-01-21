b726eab39debe7e80b080f0a9b299fb57f8a7712
Brent Ahrens <cidida@gmail.com>
2019-01-21 12:46:20 -0800

    Set up server

---
+++ server.js
@@ -0,0 +1,16 @@
+//requires
+
+const express = require('express'); 
+const app = express(); 
+const bodyParser = require('body-parser'); 
+const methodOverride = require('method-override'); 
+
+//middleware
+
+//routers 
+
+//listener
+
+app.listen(3000, function() {
+    console.log('listening on port 3000')
+})
